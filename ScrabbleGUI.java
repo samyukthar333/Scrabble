e0f008d6141ff5a5d8e13299016e073ed6770352
sramanan007 <sramanan007@lhs.win.fuhsd.org>
2019-05-07 12:15:30 -0700

    GUI class

---
+++ ScrabbleGUI.java
@@ -0,0 +1,18 @@
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JScrollPane;
+
+public class ScrabbleGUI extends JPanel
+{
+
+    /**
+     * Create the panel.
+     */
+    public ScrabbleGUI()
+    {
+        setLayout(new GridLayout(1, 0, 0, 0));
+
+    }
+
+}
