7d33444d8a4dea22e7613fcd80b27f0b33511226
Kajihana <saanvi.shreesha@gmail.com>
2019-05-30 17:38:41 -0700

    comments

--- ScrabbleFrontEnd.java
+++ ScrabbleFrontEnd.java
@@ -10,9 +10,7 @@ import javax.swing.JFrame;
 
 /**
  * 
- *  TODO Write a one-sentence summary of your class here.
- *  TODO Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
+ *  executes the visuals of the gameboard
  *
  *  @author  Richa, saanvi, samyuktha
  *  @version May 22, 2019
